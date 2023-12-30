87fb045457b82bac26471ae590abeefee1cd9ce0
kamalbuilds <geniusamansingh@gmail.com>
2023-12-30 20:30:29 +0530

    mint and rent nft pg added

---
+++ const/contractAddresses.ts
@@ -0,0 +1,16 @@
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { AerochainTestnet} from "@thirdweb-dev/chains";
+
+export const NETWORK = AerochainTestnet;
+
+// 2. The address of the marketplace V3 smart contract.
+export const MARKETPLACE_ADDRESS = "";
+
+// 3. The address of your NFT collection smart contract.
+export const NFT_COLLECTION_ADDRESS = "";
+
+export const RENTNFT_COLLECTION_ADDRESS = "";
+
+// (Optional) Set up the URL of where users can view transactions on
+export const ETHERSCAN_URL = "https://aeroscan.com/";
