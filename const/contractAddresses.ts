b50a6741d2a9fa5a3db5451c4a5186f52106c000
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-01-28 03:50:33 +0530

    rent cotnract testingg

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -10,7 +10,7 @@ export const MARKETPLACE_ADDRESS = "0x4720e2926c360250C6930174241fA2EB0fBaBC29";
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS = "0x3e508632189D6b211F3c9b96B429B7B5d22704DA";
 
-export const RENTNFT_COLLECTION_ADDRESS = "";
+export const RENTNFT_COLLECTION_ADDRESS = "0x4492299E1537b08aEdC0653Fd139A28BEFbd3300";
 
 // (Optional) Set up the URL of where users can view transactions on
 export const ETHERSCAN_URL = "https://aeroscan.com/";
