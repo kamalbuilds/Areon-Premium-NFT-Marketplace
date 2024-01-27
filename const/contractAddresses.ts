930ed0b1bf14c5ec9294a65be006f0036d39d7ca
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-01-28 03:48:35 +0530

    deploy contracts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -5,10 +5,10 @@ import { AerochainTestnet} from "@thirdweb-dev/chains";
 export const NETWORK = AerochainTestnet;
 
 // 2. The address of the marketplace V3 smart contract.
-export const MARKETPLACE_ADDRESS = "";
+export const MARKETPLACE_ADDRESS = "0x4720e2926c360250C6930174241fA2EB0fBaBC29";
 
 // 3. The address of your NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS = "";
+export const NFT_COLLECTION_ADDRESS = "0x3e508632189D6b211F3c9b96B429B7B5d22704DA";
 
 export const RENTNFT_COLLECTION_ADDRESS = "";
 
