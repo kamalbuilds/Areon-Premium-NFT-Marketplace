b3f95bcdd688e00ff98f20c4d1e8629a82473bda
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-01-28 07:04:53 +0000

    update the packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,29 @@ import { Navbar } from "../components/Navbar/Navbar";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
-import { AerochainTestnet} from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={AerochainTestnet}>
+    <ThirdwebProvider 
+    activeChain={{
+      // === Required information for connecting to the network === \\
+      chainId: 462, // Chain ID of the network
+      // Array of RPC URLs to use
+      rpc: ["https://testnet-rpc5.areon.network"],
+
+      // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+      // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+      nativeCurrency: {
+        decimals: 18,
+        name: "TAREA",
+        symbol: "TAREA",
+      },
+      shortName: "areatestnet", // Display value shown in the wallet UI
+      slug: "areatestnet", // Display value shown in the wallet UI
+      testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+      chain: "AREAtestnet", // Name of the network
+      name: "Area Testnet", // Name of the network
+}} >
       {/* Progress bar when navigating between pages */}
       <NextNProgress
         color="var(--color-tertiary)"
