ef0b6b14cd8d7b90e0b55012372994a971960c78
kamalbuilds <geniusamansingh@gmail.com>
2024-01-30 13:45:29 +0530

    UI improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,12 +26,28 @@ const Home: NextPage = () => {
           </div>
           <div className={styles.heroAssetFrame}>
             <Image
-              src="/home.png"
-              width={650}
+              src="/dolphin.png"
+              width={450}
               height={540}
               alt="Hero asset, NFT marketplace"
               quality={100}
               className={styles.heroAsset}
+            />
+            <Image
+              src="/alligator.png"
+              width={600}
+              height={500}
+              alt="Prince"
+              quality={100}
+              className={styles.gradient}
+            />
+            <Image
+              src="/seaareon.png"
+              width={450}
+              height={540}
+              alt="Background gradient from red to blue"
+              quality={100}
+              // className={styles.gradient}
             />
           </div>
           <div className={styles.heroBodyContainer}>
@@ -43,29 +59,32 @@ const Home: NextPage = () => {
                 <br />
               </h1>
               <h2>
-                Trade Unique Premium NFT Collections on Areon's Premium Marketplace 🌐
+                Trade Unique Premium NFT Collections on Areon's Premium
+                Marketplace 🌐
               </h2>
               <br />
             </div>
           </div>
         </div>
         <div className="text-center mx-56">
-            <p className={styles.heroSubtitle}>
-              Seamlessly trade, collect, and explore unique NFTs while having access to comprehensive transaction history. Immerse yourself in the future of gaming and unlock the true potential of digital ownership.
-            </p>
-                <div className={styles.heroCtaContainer}>
-                  <Link className={styles.heroCta} href="/buy">
-                    Get Started
-                  </Link>
-                  <Link
-                    className={styles.secondaryCta}
-                    href="https://github.com/kamalbuilds/areongaminghub"
-                    target="_blank"
-                  >
-                    GitHub
-                  </Link>
-                </div>
-              </div>
+          <p className={styles.heroSubtitle}>
+            Seamlessly trade, collect, and explore unique NFTs while having
+            access to comprehensive transaction history. Immerse yourself in the
+            future of areons and unlock the true potential of digital ownership.
+          </p>
+          <div className={styles.heroCtaContainer}>
+            <Link className={styles.heroCta} href="/buy">
+              Get Started
+            </Link>
+            <Link
+              className={styles.secondaryCta}
+              href="https://github.com/kamalbuilds/Areon-Premium-NFT-Marketplace"
+              target="_blank"
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
